4900c36483295f0342577112e985c06d09590f2b
Filipe Rodrigues <filipe.rds.dev@gmail.com>
2026-07-29 15:09:21 -0300

    feat: enhance Lambda handlers for Admin and Customer with structured JSON responses

--- src/serverless_ticketing_notifications/ticketing/infrastructure/http/customer/handler.py
+++ src/serverless_ticketing_notifications/ticketing/infrastructure/http/customer/handler.py
@@ -1,3 +1,11 @@
+import json
+
 
 def lambda_handler(event, context):
-    return {"statusCode": 200, "body": {"message": 'Hello from Lambda!'}}
+    return {
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": json.dumps(
+            {"controller": "Customer", "message": "Customer controller is running!"}
+        ),
+    }
